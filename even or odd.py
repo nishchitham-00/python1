d0ecd053c0c2fecb59f48898a33e992a5366fb42
nishchitha m <nishunishchitha355@gmail.com>
2026-04-09 20:37:02 +0530

    Add files via upload

---
+++ even or odd.py
@@ -0,0 +1,6 @@
+number=int(input("enter number :"))
+
+if number %2==0 :
+ print(f"the number {number} is even.")
+else:
+  print(f"the number {number} is odd. ")
---
+++ sample input formatted ouput.py
@@ -0,0 +1,10 @@
+simple data input and formatted output
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+print(f"Hello {name}, you are {age} years old.")
+
+ouput
+
+///Enter your name: nishchitha
+Enter your age: 20
+Hello nishchitha, you are 20 years old.///
